4820dedb40292175085cfe224c880c77f68884dc
Dale Peakall <dale@peakall.net>
2019-10-03 20:36:12 +0100

    Added an extra stage

--- colorui/docker-entrypoint.v4.sh
+++ colorui/docker-entrypoint.v4.sh
@@ -13,9 +13,22 @@ iptables -t nat -A PREROUTING -p tcp -j ENVOY_REDIRECT
 # Process outbound traffic
 iptables -t nat -N ENVOY_OUTPUT
 iptables -t nat -A OUTPUT -j ENVOY_OUTPUT
+iptables -t nat -A ENVOY_OUTPUT -p tcp --dport 53 -j RETURN # don't redirect DNS traffic
 iptables -t nat -A ENVOY_OUTPUT -m owner --uid-owner 1337 -j RETURN # don't redirect traffic from envoy user
 iptables -t nat -A ENVOY_OUTPUT -m owner --gid-owner 1337 -j RETURN # don't redirect traffic from envoy group
 iptables -t nat -A ENVOY_OUTPUT -j ENVOY_REDIRECT
+
+cat<<EOF >/etc/resolv.conf
+nameserver=127.0.0.1
+options ndots:0
+EOF
+
+cat<<EOF >/etc/dnsmasq.d/srv.conf
+address=/srv/127.0.0.1
+server=127.0.0.11
+EOF
+
+dnsmasq
 
 su-exec envoy:envoy envoy -c /etc/envoy/envoy.yaml --service-node $(hostname) &
 exec su-exec app:app ./colorui "$@"
